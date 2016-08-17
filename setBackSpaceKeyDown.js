a1f82bed7d2a1ed5578ed28b49c75982cb357b13
KimYoungWoong <kimyw42@naver.com>
2016-08-17 10:49:17 +0900

    20160817-3

--- setBackSpaceKeyDown.js
+++ setBackSpaceKeyDown.js
@@ -1,18 +1,13 @@
 document.onkeydown = function(event){
     if(event.keyCode !== 8){
         return;
+    }else{
+        var focus = event.path[0];
+        var passFiled = /INPUT/OPTION/SELECT/TEXTAREA/i;
+
+        if(passFiled.test(focus) && !document.activeElement.isContentEditable){
+            event.preventDefault();
+            window.history.back(-1);
+        }
     }
-    
-    const focus = event.path[0];
-    
-    if(['INPUT','TEXTAREA','SELECT','FILED'].indexOf(focus.tagName) > -1){
-        return;
-    }
-    
-    if(document.activeElement.isContentEditable){
-        return;
-    }
-    
-    event.preventDefault();
-    window.history.back(-1);
 } 
